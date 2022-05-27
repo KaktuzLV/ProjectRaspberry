8c4c3c4a107d0bf06aaae45969a04c1b7f608b70
KaktuzLV <psilvestrs@gmail.com>
2022-05-27 14:36:47 +0300

    Python package...

---
+++ sensor/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sensor',
+    version='0.1',
+    description='Testing installation of Package Sensor',
+    url='#',
+    author='Silvestrs',
+    author_email='psilvestrs@gmail.com',
+    packages=['sensor', 'sensor.dht11'],
+    zip_safe=False,
+    install_requires=[],
+)
+
+
