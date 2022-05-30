0383990e1b1ced987257b0f7d8b47d7575122ef3
bubafistah <somewhere@fake>
2022-05-30 13:07:49 +0300

    Add __main__ check

--- src/sensor/main.py
+++ src/sensor/main.py
@@ -2,7 +2,8 @@ from dht11 import dht11Sensor
 from dht11 import sqlInsert
 import time
 
-while True:
-    temp, humid, date = dht11Sensor.getSensorReading()
-    sqlInsert.insertReadingFromSensor(temp, humid, date)
-    time.sleep(10)
+if __name__ == "__main__":
+    while True:
+        temp, humid, date = dht11Sensor.getSensorReading()
+        sqlInsert.insertReadingFromSensor(temp, humid, date)
+        time.sleep(10)
